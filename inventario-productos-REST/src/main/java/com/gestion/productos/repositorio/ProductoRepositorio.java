f7e27ee6ce6c1a9c9f2da4d58090474651cbec32
Gino Paoletti <83784311+gipage@users.noreply.github.com>
2022-08-08 20:23:04 -0300

    Add files via upload

---
+++ inventario-productos-REST/src/main/java/com/gestion/productos/repositorio/ProductoRepositorio.java
@@ -0,0 +1,7 @@
+package com.gestion.productos.repositorio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.gestion.productos.modelo.Producto;
+
+public interface ProductoRepositorio extends JpaRepository<Producto,Integer> {
+
+}
